60c9db7943538c2d58c3e80c9e599fd4f4e60552
johnpooler <johnpooler98@gmail.com>
2020-03-24 17:09:50 +0000

    Model movie added & template database TODO

--- app/controllers/MoviesGallery.scala
+++ app/controllers/MoviesGallery.scala
@@ -2,11 +2,29 @@ package controllers
 
 import akka.http.scaladsl.model.HttpHeader.ParsingResult.Ok
 import controllers.movieDatabase
+import javax.inject.Inject
 import play.api._
+import play.api.mvc.{AbstractController, ControllerComponents}
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 import play.mvc.Controller
 import play.mvc.Action
 
-class MoviesGallery extends Controller{
+class MoviesGallery @Inject()(
+                               components: ControllerComponents,
+                               val reactiveMongoApi: ReactiveMongoApi
+                             ) extends AbstractController(components)
+  with MongoController with ReactiveMongoComponents {
+
+
+
+
+
+
+
+
+
+
+
   def allMovies = Action {
 
     Ok(views.html.allMoviesGallery(movieDatabase.allFilmsList))
