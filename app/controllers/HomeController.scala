5a0f6f8c22e567026085769801b10a61752654c2
EviNikolaidou <Evi.Nikolaidou@outlook.com>
2020-03-24 15:03:24 +0000

    design of classifications is done

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -20,4 +20,8 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def classifications() = Action {
+    Ok(views.html.classifications("Movie Classifications"))
+  }
+
 }
