0e41952c2260b96ed7c86b16c958da3a85a4077c
malik <malikbensalem@outlook.com>
2020-03-25 14:23:23 +0000

    opening-times (contact page) needs email contact form

---
+++ app/controllers/contactController.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+@Singleton
+class ContactController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  def contact = Action {
+    Ok(views.html.c("Your new application is ready."))
+  }
+}
