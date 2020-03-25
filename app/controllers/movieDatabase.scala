67f81950a7fb4afb4afc3aee408db3d0ba1c3605
johnpooler <johnpooler98@gmail.com>
2020-03-25 11:35:24 +0000

    Movie INFO added

--- app/controllers/movieDatabase.scala
+++ app/controllers/movieDatabase.scala
@@ -9,11 +9,10 @@ object movieDatabase {
   val allFilmsList :List[Movie] = List[Movie](
     new Movie(1,"Anchorman 1", "Comedy", "https://images-na.ssl-images-amazon.com/images/I/91TXzLjVV-L._SL1500_.jpg", List("Will Ferrel", "John"),
       "Tarantino" ),
-    new Movie(1,"Anchorman 2", "Comedy", "https://images-na.ssl-images-amazon.com/images/I/81qxP6kFOeL._SY445_.jpg", List("Jim Carrey", "John"),
+    new Movie(2,"Anchorman 2", "Comedy", "https://images-na.ssl-images-amazon.com/images/I/81qxP6kFOeL._SY445_.jpg", List("Jim Carrey", "John"),
       "Tarantino" ),
-    new Movie(1,"BruceAllMighty", "Comedy", "https://resizing.flixster.com/8GO-zLY5jL4_peZ_bvGUZkpC3i0=/206x305/v1.bTsxMTE3NjUyNDtqOzE4NDQ0OzEyMDA7ODAwOzEyMDA", List("Jennifer Anniston"),
+    new Movie(3,"BruceAllMighty", "Comedy", "https://resizing.flixster.com/8GO-zLY5jL4_peZ_bvGUZkpC3i0=/206x305/v1.bTsxMTE3NjUyNDtqOzE4NDQ0OzEyMDA7ODAwOzEyMDA", List("Jennifer Anniston"),
       "Tarantino" )
-
 
   )
 
