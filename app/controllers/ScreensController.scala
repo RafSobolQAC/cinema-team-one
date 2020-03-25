bb5c7d4b67d83698a287c0829090c11527e0b385
EviNikolaidou <Evi.Nikolaidou@outlook.com>
2020-03-25 14:05:46 +0000

    screen added todo comment

--- app/controllers/ScreensController.scala
+++ app/controllers/ScreensController.scala
@@ -2,11 +2,11 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 
+
 class ScreensController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
 
   def screens() = Action {
     Ok(views.html.screens("Screens"))
   }
-
 }
