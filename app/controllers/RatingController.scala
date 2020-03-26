feb634d34827c91411aa31c8e4cfe1d62ff17bc8
dannywaughman <dannywaughman@gmail.com>
2020-03-26 15:18:22 +0000

    Working on new ratings branch

--- app/controllers/RatingController.scala
+++ app/controllers/RatingController.scala
@@ -1,5 +1,34 @@
 package controllers
 
-class RatingController {
+import javax.inject.Inject
+import models.Rating
+import play.api.mvc._
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import services.ReleasedServices
+
+import scala.concurrent.Future
+
+class RatingController  @Inject()(
+                                   components: ControllerComponents,
+                                   val reactiveMongoApi: ReactiveMongoApi,
+                                   val releasedServices: ReleasedServices
+                                 ) extends AbstractController(components)
+  with MongoController with ReactiveMongoComponents with play.api.i18n.I18nSupport {
+
+  def getRating = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.rating(Rating.createRating))
+  }
+
+  def submitRating = Action.async {implicit request: Request[AnyContent] =>
+    Rating.createRating.bindFromRequest.fold({formWithErrors =>
+      println("This is the form with errors")
+      println(formWithErrors)
+      Future.successful(BadRequest(views.html.rating(formWithErrors)))
+    }, {rating =>
+      releasedServices.getMovies(rating).map(_ => {
+        Redirect(routes.RatingController.submitRating).flashing("success" -> "Successfully created!")
+      })
+    })
+  }
 
 }
