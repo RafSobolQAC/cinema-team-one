6db9cbd1eefdbc5fee4a0479ff9279493f685daf
malik <malikbensalem@outlook.com>
2020-03-30 11:59:09 +0100

    added testing to ContactController

---
+++ test/controllers/ContactControllerTest.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import org.scalatestplus.play._
+import play.api.test.Helpers._
+import play.api.mvc._
+import play.api.test._
+import scala.concurrent.Future
+
+
+class ContactControllerTest extends PlaySpec with Results{
+  "ContactController" should {
+    "Show the Contact page" in {
+      val controller= new ContactController(Helpers.stubControllerComponents())
+      val result: Future[Result] = controller.contact.apply(FakeRequest())
+      contentType(result) mustBe Some("text/html")
+      contentAsString(result) must include("Opening Times")
+      contentAsString(result) must include("Visit Us")
+    }
+  }
+}
