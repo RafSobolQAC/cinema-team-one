4dde9cf583b0804ff286cf21ce00611495ae50fc
nathan-rees <poohplop1@gmail.com>
2020-03-31 23:24:29 +0100

    changes to payment to make it easier to manage (for u raf <3)

--- app/controllers/BookingController.scala
+++ app/controllers/BookingController.scala
@@ -1,23 +1,18 @@
 package controllers
 
-import javax.inject.{Inject, _}
+import javax.inject.Inject
 import models.Booking
 import play.api.mvc._
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
-import models.JsonFormats._
-import play.api.libs.json.{JsValue, Json}
-import reactivemongo.play.json.collection.JSONCollection
 import services.{BookingServices, ReleasedServices}
 
-import scala.collection.mutable.ListBuffer
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.io.Source
+import scala.concurrent.{ExecutionContext, Future}
 
 class BookingController @Inject()(
                                  components: ControllerComponents,
                                  val reactiveMongoApi: ReactiveMongoApi,
                                  val bookingServices: BookingServices,
+                                 val paymentcontroller:PaymentController,
                                  val releasedServices: ReleasedServices
                                ) extends AbstractController(components)
   with MongoController with ReactiveMongoComponents with play.api.i18n.I18nSupport {
@@ -25,14 +20,15 @@ class BookingController @Inject()(
   implicit def ec: ExecutionContext = components.executionContext
 
   def showForm = Action {implicit request: Request[AnyContent] =>
-    Ok(views.html.booking(Booking.createBookingForm))
+    Ok(views.html.booking(Booking.createBookingForm,paymentcontroller.createOrder(5,"")._1))
   }
+
 
   def submitForm = Action.async {implicit request: Request[AnyContent] =>
     Booking.createBookingForm.bindFromRequest.fold({formWithErrors =>
       println("This is the form with errors!")
       println(formWithErrors)
-      Future.successful(BadRequest(views.html.booking(formWithErrors)))
+      Future.successful(BadRequest(views.html.booking(formWithErrors,null)))
     }, {booking =>
       bookingServices.createBooking(booking).map(_ => {
         Redirect(routes.BookingController.showForm).flashing("success" -> "Successfully created!")
