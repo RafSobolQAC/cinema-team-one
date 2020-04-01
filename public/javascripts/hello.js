155921df66c8d992bdc5904d8d74f5524d27719e
malik <malikbensalem@outlook.com>
2020-04-01 10:03:34 +0100

    fixed merge conflicts

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,11 @@
 if (window.console) {
   console.log("Welcome to your Play application's JavaScript!");
 }
+
+
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
