e520e8f4d17bca9a2d113dd2938016eb5ad0f7b2
nathan-rees <poohplop1@gmail.com>
2020-04-01 10:46:49 +0100

    changes to make integration easier

--- app/controllers/BookingController.scala
+++ app/controllers/BookingController.scala
@@ -9,22 +9,30 @@ import services.{BookingServices, ReleasedServices}
 import scala.concurrent.{ExecutionContext, Future}
 
 class BookingController @Inject()(
-                                 components: ControllerComponents,
-                                 val reactiveMongoApi: ReactiveMongoApi,
-                                 val bookingServices: BookingServices,
-                                 val paymentcontroller:PaymentController,
-                                 val releasedServices: ReleasedServices
+                                   components: ControllerComponents,
+                                   val reactiveMongoApi: ReactiveMongoApi,
+                                   val bookingServices: BookingServices,
+                                   val paymentController:PaymentController,
+                                   val releasedServices: ReleasedServices
                                ) extends AbstractController(components)
   with MongoController with ReactiveMongoComponents with play.api.i18n.I18nSupport {
 
+  val pay=paymentController.createOrder(5,"http://localhost:9099/capturePayment")
   implicit def ec: ExecutionContext = components.executionContext
 
   def showForm = Action {implicit request: Request[AnyContent] =>
-    Ok(views.html.booking(Booking.createBookingForm,paymentcontroller.createOrder(5,"")._1))
+    println(pay._1)
+    Ok(views.html.booking(Booking.createBookingForm,pay._1))
+
   }
 
 
   def submitForm = Action.async {implicit request: Request[AnyContent] =>
+//    if(paymentController.capturePayment(pay._2)==Ok("Order completed")){
+//        Ok("what happens when purchase is verified")
+//    }else{
+//      Ok("pay the moni")
+//    }
     Booking.createBookingForm.bindFromRequest.fold({formWithErrors =>
       println("This is the form with errors!")
       println(formWithErrors)
